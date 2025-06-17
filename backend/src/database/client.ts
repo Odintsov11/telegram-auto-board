6e699181d3589692f3fd72c7a9efe4526eebbf0f
Roman Odintsov <rom4ikk2710@gmail.com>
2025-06-17 11:11:24 +0200

    fix: use relative import for logger in database client

--- backend/src/database/client.ts
+++ backend/src/database/client.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { logger } from '@/utils/logger'
+import { logger } from '../utils/logger'
 
 export const prisma = new PrismaClient({
   log: ['query', 'info', 'warn', 'error'],
